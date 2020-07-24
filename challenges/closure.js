66b4bef5aac86054e7588314770d7e5a7c1539c3
David Gold <davidgold@hey.com>
2020-07-24 11:29:26 -0400

    updates

--- challenges/closure.js
+++ challenges/closure.js
@@ -17,8 +17,19 @@ function myFunction() {
 myFunction();
 
 // Explanation: 
-
+By nesting a function, the nestedFunction() creates a closure. This allows the function to reach out into its surrounding parts (it's lexical environment) to pull in variables much like a normal function would from global scope. 
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+function summation(num) {
+  let counter = 0;
+  return function summation2(num) {
+    for (let i = 0; i <= num; i++) {
+      counter += i;
+    } return counter;
+  } 
+}
+
+
+console.log('task 2', summation(4));
